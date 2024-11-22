32d68aa5e854c0b5fe7b1c00f1f9f51d6cc4405d
anmol983 <anmol.21ug2049@iiitranchi.ac.in>
2024-11-23 01:50:10 +0530

    ui changes

--- lib/main_injection_container.dart
+++ lib/main_injection_container.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
@@ -10,7 +9,6 @@ import 'package:whatsapp_clone_app/features/user/user_injection_container.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-
   final auth = FirebaseAuth.instance;
   final fireStore = FirebaseFirestore.instance;
 
@@ -21,5 +19,4 @@ Future<void> init() async {
   await chatInjectionContainer();
   await statusInjectionContainer();
   await callInjectionContainer();
-
 }
